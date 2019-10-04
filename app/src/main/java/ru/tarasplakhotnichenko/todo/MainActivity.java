80facd3f8e65a34e0a669730664b1557a9370ba8
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-04 16:57:28 +0300

    Android для начинающих. Урок 4. Recycler View.

---
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/MainActivity.java
@@ -0,0 +1,73 @@
+package ru.tarasplakhotnichenko.todo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+//import android.support.annotation.NonNull;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+import android.widget.Switch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private final List<Item> items = new ArrayList<>();
+    private final RecyclerView.Adapter adapter = new ItemAdapter(this.items);;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recycler = findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adapter);
+    }
+
+    public void add(View view) {
+        EditText edit = this.findViewById(R.id.editText);
+        this.items.add(new Item(edit.getText().toString()));
+        edit.setText("");
+        adapter.notifyItemInserted(this.items.size() - 1);
+    }
+
+    private static final class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private final List<Item> items;
+
+        public ItemAdapter(List<Item> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int index) {
+            return new RecyclerView.ViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.items, parent, false)
+            ) {};
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int index) {
+            TextView name = holder.itemView.findViewById(R.id.name);
+            name.setText(String.format("%s. %s", index, this.items.get(index).getName()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return this.items.size();
+
+        }
+    }
+}
