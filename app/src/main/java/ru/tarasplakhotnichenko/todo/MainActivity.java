e33822f7d34736bc7659dd55077bddb5000b6632
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-09 13:54:45 +0300

    Recycler widget HowTo

--- app/src/main/java/ru/tarasplakhotnichenko/todo/MainActivity.java
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/MainActivity.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private final List<Item> items = new ArrayList<>();
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void add(View view) {
         EditText edit = this.findViewById(R.id.editText);
-        this.items.add(new Item(edit.getText().toString()));
+        this.items.add(new Item(edit.getText().toString(), Calendar.getInstance()));
 
         edit.setText("");
         adapter.notifyItemInserted(this.items.size() - 1);
@@ -57,8 +60,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int index) {
             TextView name = holder.itemView.findViewById(R.id.name);
+            TextView created = holder.itemView.findViewById(R.id.created);
+            Item item = this.items.get(index);
+            name.setText(String.format("%s. %s", index, item.getName()));
+            created.setText(format(item.getCreated()));
+
+            CheckBox done = holder.itemView.findViewById(R.id.done);
+            done.setOnCheckedChangeListener((view, checked) item.setDone(checked));
+
+            private String format(Calendar cal) {
+                return String.format(
+                        Locale.getDefault(), "%02d.%02d.%d",
+                        cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH),cal.get(Calendar.YEAR)
+                );
+            }
 
             //---------------------------------------------------------------------
+            /*
             Button deleteButton = holder.itemView.findViewById(R.id.delete);
             deleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -66,10 +84,11 @@ public class MainActivity extends AppCompatActivity {
                     delete(index);
                 }
             });
-
+            name.setText(String.format("%s. %s", index, this.items.get(index).getName()));
+             */
             //---------------------------------------------------------------------
 
-            name.setText(String.format("%s. %s", index, this.items.get(index).getName()));
+
         }
 
         @Override
