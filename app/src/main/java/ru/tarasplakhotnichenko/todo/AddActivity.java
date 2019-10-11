abd923cfd35462b54c56a60de403ce4b65e43297
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-11 17:21:57 +0300

    lesson 7: intent with args

--- app/src/main/java/ru/tarasplakhotnichenko/todo/AddActivity.java
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/AddActivity.java
@@ -1,14 +1,12 @@
 package ru.tarasplakhotnichenko.todo;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-
-
+import android.widget.TextView;
 
 import java.util.Calendar;
 
@@ -18,6 +16,23 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle saved) {
         super.onCreate(saved);
         setContentView(R.layout.add);
+
+
+        //Mine------------
+
+        int indexEdit = getIntent().getIntExtra("indexEdit", 0);
+        boolean intentEditYes = getIntent().getBooleanExtra("indexEditYes", false);
+        if (intentEditYes) {
+            Item item = Store.getStore().get(indexEdit);
+
+            TextView name = findViewById(R.id.name);
+            name.setText(item.getName());
+
+            TextView desc = this.findViewById(R.id.desc);
+            desc.setText(item.getDesc());
+        }
+
+
     }
 
     public void save(View view) {
