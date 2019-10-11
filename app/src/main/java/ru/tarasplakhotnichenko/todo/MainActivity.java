7346012a295ccd59d9dee42d18483a0ea1880a5e
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-11 14:34:44 +0300

    lesson 7: intent with args

--- app/src/main/java/ru/tarasplakhotnichenko/todo/MainActivity.java
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/MainActivity.java
@@ -1,22 +1,12 @@
 package ru.tarasplakhotnichenko.todo;
 
 import android.content.Intent;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
     private final RecyclerView.Adapter adapter = new ItemAdapter(this);
