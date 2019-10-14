d9670cb94ffbb3c5bb556f3b70cb5094b40c47ce
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-14 17:13:00 +0300

    fragments

--- app/src/main/java/ru/tarasplakhotnichenko/todo/ViewFragmentActivity.java
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/ViewFragmentActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+//
 public class ViewFragmentActivity extends BaseActivity {
     @Override
     public Fragment loadFragment() {
