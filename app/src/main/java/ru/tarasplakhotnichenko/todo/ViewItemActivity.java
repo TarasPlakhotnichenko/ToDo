4be93f6a9fd67b7b5704c098c12b13f71c260d05
TarasPlakhotnichenko <Taras.Plakhotnichenko@gmail.com>
2019-10-15 16:02:55 +0300

    lesson_8_frg_with_args

--- app/src/main/java/ru/tarasplakhotnichenko/todo/ViewItemActivity.java
+++ app/src/main/java/ru/tarasplakhotnichenko/todo/ViewItemActivity.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment;
 public class ViewItemActivity extends BaseActivity {
     @Override
     public Fragment loadFrg() {
-        return new ViewItemFrg();
+        int index = getIntent().getIntExtra("index", 0);
+        return ViewItemFrg.instOf(index);
+        //return new ViewItemFrg();
     }
 }
